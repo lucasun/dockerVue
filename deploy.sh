28a342848c3b93457ed214720c9d6da74b0efdd9
sunchao <joysunchao@didiglobal.com>
2020-01-08 20:13:26 +0800

    modify config

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ echo $image_version;
 git pull --rebase origin master;
 docker stop dockerVue;
 docker rm dockerVue;
-docker build -t dockerVue:$image_version .;
+docker build -t dockervue:$image_version .;
 docker images;
-docker run -p 10002:80 -d --name dockerVue dockerVue:$image_version;
+docker run -p 9999:80 -d --name dockerVue dockervue:$image_version;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
-docker logs vue2docker;
+docker logs dockervue;
 #删除build过程中产生的镜像    #docker image prune -a -f
 docker rmi $(docker images -f "dangling=true" -q)
