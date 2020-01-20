60da5f597b55ef4c02bc31a4adc58eef0cbd93d4
sunchao <joysunchao@didiglobal.com>
2020-01-20 17:05:24 +0800

    fix

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 git pull --rebase origin master
 yarn install --registry=https://registry.npm.taobao.org
 yarn build
+rm -rf server/node_modules
 rm -rf webapp.tar
 rm -rf server/app/public/*
 cp -r dist/* server/app/public
